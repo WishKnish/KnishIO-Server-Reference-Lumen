fd0e16db504723cc84a89ce68209a673f8010a0a
Zaur <len1in@yandex.ru>
2020-09-02 15:24:42 +0300

    Minor changes to CorsMiddleware

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -37,6 +37,9 @@ return [
             // middleware, this delegates auth and permission checks to the field level.
             // If you want to use another guard, change the suffix (remove for default).
             \Nuwave\Lighthouse\Support\Http\Middleware\AttemptAuthentication::class . ':api',
+
+            // @todo Uncomment to implement access token
+            // \WishKnish\KnishIO\GraphQL\Middleware\BundleAccessMiddleware::class,
         ],
 
         /*
