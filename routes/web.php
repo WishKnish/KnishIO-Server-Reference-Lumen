e3c1b43edc4a066c0100c9877c7e024b1b97906d
vladimir <vladimir.php@gmail.com>
2020-10-14 14:48:00 +0300

    Add scope url to get json-ld data for meta instance

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,51 @@
 |
 */
 
+
+use WishKnish\KnishIO\Client\Meta;
+use WishKnish\KnishIO\Models\Atom;
+use WishKnish\KnishIO\Models\Token;
+
+
+$router->get( 'scope/{metaType}/{metaId}.jsonld', function( $metaType, $metaId ) use ( $router ) {
+
+    // Get a meta instance
+    $atom = Atom::where( 'isotope', 'C' )
+        ->where( 'meta_type', $metaType )
+        ->where( 'meta_id', $metaId )
+        ->first();
+    if ( !$atom ) {
+        throw new \Exception( 'Meta instance does not found.' );
+    }
+
+    // Get metas
+    $metas = Meta::aggregateMeta($atom->metas);
+    if ( !array_has( $metas, 'context' ) ) {
+        throw new \Exception( 'Instance does not have a context.' );
+    }
+
+    // Get a model
+    $model = null;
+    switch( $atom->meta_type ) {
+        case 'token':
+            $model = Token::where( 'slug', $atom->meta_id )->first();
+            break;
+    }
+    if ( !$model ) {
+        throw new \Exception( 'Model does not found.' );
+    }
+
+    // All metas to output
+    $metas = array_merge( $model->getAttributes(), $metas );
+
+    // Get a meta context & filter aggregated metas
+    header( 'application/ld+json' );
+    $metaContext = new \WishKnish\KnishIO\Models\Meta\MetaContext( array_get( $metas, 'context' ) );
+    echo $metaContext->filter( $metas );
+
+});
+
+
 $router->get( '/', function () use ( $router ) {
     return view( 'index' );
 } );
