c83a2d29f10f1a792d0cc9a808955ef9f9e3bdd6
Vladimir Makarov <vladimir.php@gmail.com>
2021-07-19 14:53:05 +0300

    Add GITHUB_AUTH_TOKEN check in bootstrap.sh script

--- docker-compose/bootstrap.sh
+++ docker-compose/bootstrap.sh
@@ -2,21 +2,27 @@
 
 echo '#################### BEGIN: bootstrap.sh ####################'
 
-# Composer install
-echo 'Composer install...'
-composer config --global github-oauth.github.com $3
-composer install
+if [ "$3" ]; then
 
-echo 'Waiting while DB will be initialized...'
-while ! mysql -h"knishio-db" -u"$1" -p"$2"  -e ";" ; do
-    echo "Try to connect username=$1 password=$2..."
-    sleep 5
-done
+    # Composer install
+    echo 'Composer install...'
+
+    composer config --global github-oauth.github.com $3
+    composer install
+
+    echo 'Waiting while DB will be initialized...'
+    while ! mysql -h"knishio-db" -u"$1" -p"$2"  -e ";" ; do
+        echo "Try to connect username=$1 password=$2..."
+        sleep 5
+    done
 
 
-echo 'Starting bootstrap process...'
-php artisan migrate
+    echo 'Starting bootstrap process...'
+    php artisan migrate
 
+else
+    echo 'GITHUB_AUTH_TOKEN env var is not set.'
+fi
 
 echo '#################### END: bootstrap.sh ####################'
 exec /bin/sh -c php-fpm
