d1204f92bf4797b457b3e94b181a0f9792bfdf78
Eugene Teplitsky <eugene@wishknish.com>
2021-10-01 14:18:23 -0400

    Correcting benchmark auth token request

--- app/Console/Commands/BenchmarkCommand.php
+++ app/Console/Commands/BenchmarkCommand.php
@@ -217,7 +217,7 @@ class BenchmarkCommand extends Command {
 
             // Defining client and authenticating the session
             $client = new KnishIOClient( $this->graphql_url );
-            $client->getAuthToken( $secret );
+            $client->requestAuthToken( $secret );
 
             // $instance->info( 'Creating '. $this->molecules_count.' molecules for bundle ' . $bundle . '...' );
 
@@ -244,7 +244,7 @@ class BenchmarkCommand extends Command {
             // Defining client and authenticating the session
             $secret = Crypto::generateSecret();
             $client = new KnishIOClient( $self->graphql_url );
-            $client->getAuthToken( $secret );
+            $client->requestAuthToken( $secret );
 
             // Accumulate all meta types & ids
             $metaTypes = [];
